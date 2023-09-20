71a9d619ab6f6474b2415a9f54c3bd2c6495939b
Jay vaghani <jayvaghani2000@gmail.com>
2023-09-20 18:15:38 +0530

    add token

--- next-feature/pages/index.js
+++ next-feature/pages/index.js
@@ -1,15 +1,23 @@
-import NavBar from '../Components/Navbar/NavBar'
-import Head from 'next/head' 
+import NavBar from "../Components/Navbar/NavBar";
+import Head from "next/head";
+import { deleteCookie, getCookie, hasCookie, setCookie } from "cookies-next";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    setCookie("key", "value", {
+      maxAge: 60 * 24 * 24,
+      domain: `.${window.location.hostname}`,
+    });
+  }, []);
   return (
     <>
       <Head>
         <title>Home</title>
-        <meta name='keywords' content='next js tutorial' />
-        <meta name='author' content='Jay popatbhai vaghani' />
+        <meta name="keywords" content="next js tutorial" />
+        <meta name="author" content="Jay popatbhai vaghani" />
       </Head>
       <NavBar />
     </>
-  )
+  );
 }
