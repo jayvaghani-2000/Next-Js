a0cc537a10021436dd4e1f707583e49c5da71aed
Jay vaghani <jayvaghani2000@gmail.com>
2023-09-20 18:32:50 +0530

    remove code

--- next-feature/pages/pricing/plan1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const plan1 = () => {
-  return (
-    <div>plan1</div>
-  )
-}
-
-export default plan1
--- next-feature/pages/pricing/plan2.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const plan2 = () => {
-  return (
-    <div>plan2</div>
-  )
-}
-
-export default plan2
